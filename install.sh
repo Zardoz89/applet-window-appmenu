4634aea21803c81674c95cf1cf85533b0a7a4135
Shenyao Jin <shenyao_jin@zju.edu.cn>
2022-09-12 01:45:01 -0600

    Update install.sh
    Not really sure. Not tested yet.

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 if ! [ -a build ] ; then
     mkdir build
 fi
+# Add path to ~/.zshrc or ~/.bashrc
+zsh_route=/usr/bin/zsh
+bash_route=/usr/bin/bash
+if [ $SHELL == $bash_route ]
+then then echo "export KDE_INSTALL_QMLDIR="/usr/lib/qml:$KDE_INSTALL_QMLDIR"" >> ~/.bashrc
+elif [ $SHELL == $zsh_route ]
+then echo "export KDE_INSTALL_QMLDIR="/usr/lib/qml:$KDE_INSTALL_QMLDIR"" >> ~/.zshrc
+fi
+
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release  .. -Wnodev
 make -j$(nproc)
